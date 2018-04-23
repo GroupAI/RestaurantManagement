9bd0c0592959e9ab9b30e7d0380caee0d321469e
1992b <1992bidyut@gmail.com>
2018-04-23 19:03:52 +0600

    FoodList, Login,addFood with DB

--- app/src/main/java/bdnath/lictproject/info/restaurantmanagement/database/FoodDatabaseSource.java
+++ app/src/main/java/bdnath/lictproject/info/restaurantmanagement/database/FoodDatabaseSource.java
@@ -8,6 +8,8 @@ import android.database.sqlite.SQLiteDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
+import bdnath.lictproject.info.restaurantmanagement.FoodClass;
+
 public class FoodDatabaseSource {
     private FoodDatabaseHelper helper;
     private SQLiteDatabase db;
@@ -24,13 +26,13 @@ public class FoodDatabaseSource {
         db.close();
     }
 
-    public boolean insertFood(Food food){
+    public boolean insertFood(FoodClass food){
         this.open();
         ContentValues values = new ContentValues();
         values.put(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_NAME, food.getFoodName());
-        values.put(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_TYPE, food.getFoodtype());
+        values.put(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_TYPE, food.getFoodType());
         values.put(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_PRICE, food.getFoodPrice());
-        values.put(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_IMGPATH, food.getFoodImgPath());
+        values.put(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_IMGPATH, food.getImagePath());
         values.put(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_DETAILS, food.getFoodDetails());
 
         long insertedRow = db.insert(FoodDatabaseHelper.TABLE_FOOD,null, values);
@@ -42,9 +44,9 @@ public class FoodDatabaseSource {
         }
     }
 
-    public List<Food> getAllFoods(){
+    public List<FoodClass> getAllFoods(){
         this.open();
-        List<Food>foods=new ArrayList<>();
+        List<FoodClass>foods=new ArrayList<>();
 
         Cursor c = db.query(FoodDatabaseHelper.TABLE_FOOD, null, null,null,null, null,null);
         if (c!=null && c.getCount()>0){
@@ -55,10 +57,10 @@ public class FoodDatabaseSource {
                 String foodName = c.getString(c.getColumnIndex(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_NAME));
                 String foodType = c.getString(c.getColumnIndex(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_TYPE));
                 String foodPrice = c.getString(c.getColumnIndex(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_PRICE));
-                String foodImgPath = c.getString(c.getColumnIndex(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_IMGPATH);
+                String foodImgPath = c.getString(c.getColumnIndex(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_IMGPATH));
                 String foodDetails = c.getString(c.getColumnIndex(FoodDatabaseHelper.TABLE_FOOD_COL_FOOD_DETAILS));
 
-                Food food = new Food(id, foodName, foodType, foodPrice, foodImgPath, foodDetails);
+                FoodClass food = new FoodClass(id, foodName, foodType, foodPrice, foodDetails, foodImgPath);
                 foods.add(food);
             }while (c.moveToNext());
         }
